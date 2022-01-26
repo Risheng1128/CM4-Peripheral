b6837843608cd4eca145dab4db24791398d6dbaf
Risheng <hi4u29ck02@gmail.com>
2022-01-27 02:48:13 +0800

    update

--- Drivers/Src/stm32f303ze_i2c_driver.c
+++ Drivers/Src/stm32f303ze_i2c_driver.c
@@ -19,20 +19,22 @@
  *********************************************************/
 void I2C_PeriClockControl(I2C_RegDef_t* pI2Cx, uint8_t EnorDi)
 {
-    if(EnorDi == ENABLE)
+    if(EnorDi == ENABLE) {
         if(pI2Cx == I2C1)
             I2C1_PCLK_EN();
         else if(pI2Cx == I2C2)
             I2C2_PCLK_EN();
         else if(pI2Cx == I2C3)
             I2C3_PCLK_EN();
-    else if(EnorDi == DISABLE)
+    }
+    else if(EnorDi == DISABLE) {
         if(pI2Cx == I2C1)
             I2C1_PCLK_DI();
         else if(pI2Cx == I2C2)
             I2C2_PCLK_DI();
         else if(pI2Cx == I2C3)
             I2C3_PCLK_DI();
+    }
 }
 
 /********************************************************
